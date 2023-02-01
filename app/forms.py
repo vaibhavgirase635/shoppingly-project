f96c16d8958620d723853815192694160dce8f84
vaibhavgirase635 <vaibhavrajput635@gmail.com>
2023-02-01 16:41:45 +0530

    change in profile and address

--- app/forms.py
+++ app/forms.py
@@ -44,9 +44,9 @@ class CustomerProfileForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['name', 'locality', 'city', 'state', 'zipcode']
-        widgets = {'name':forms.TextInput(attrs={'class':'form-control'}),
-        'locality':forms.TextInput(attrs={'class':'form-control'}),
-        'city':forms.TextInput(attrs={'class':'form-control'}),
-        'name':forms.Select(attrs={'class':'form-control'}),
+        widgets = {'name':forms.TextInput(attrs={'class':'form-control'}),'locality':forms.TextInput(attrs=
+        {'class':'form-control'}),'city':forms.TextInput(attrs=
+        {'class':'form-control'}),'state':forms.Select(attrs=
+        {'class':'form-control'}),
         'zipcode':forms.NumberInput(attrs={'class':'form-control'})
  }
